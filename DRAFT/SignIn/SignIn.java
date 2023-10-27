4b78de38574289c04bc6574be8eda245f25542c7
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-10-27 17:46:09 +0800

    Update SignIn.java

--- DRAFT/SignIn/SignIn.java
+++ DRAFT/SignIn/SignIn.java
@@ -56,12 +56,13 @@ class UserData {
 
 public class SignIn extends JFrame {
     private static final long serialVersionUID = 1L;
-    private JPanel contentPane;
-    private JTextField textSignIn;
+    private JPanel contentBG;
+    private JTextField txtSignIn;
     private JTextField txtUser;
-    private JPasswordField passField;
+    private JPasswordField pwdField;
     private JRadioButton showPass;
     Hashtable<String, UserData> usersData;
+    Hashtable<String, String> userAdmin;
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
@@ -78,56 +79,56 @@ public class SignIn extends JFrame {
 
     public SignIn() {
         usersData = new Hashtable<String, UserData>();
-        usersData.put("User1", new UserData("User1", "Pass123", "TeamA", new String[]{"Member1", "Member2", "Member3", "Member4", "Member5"}));
-        usersData.put("User2", new UserData("User2", "Pass1234", "TeamB", new String[]{"MemberA", "MemberB", "MemberC", "MemberD", "MemberE"}));
-        usersData.put("User3", new UserData("User3", "Pass12345", "TeamC", new String[]{"MemberX", "MemberY", "MemberZ", "MemberW", "MemberV"}));
-        usersData.put("User4", new UserData("User4", "Pass123456", "TeamD", new String[]{"MemberI", "MemberII", "MemberIII", "MemberIV", "MemberV"}));
-
+        usersData.put("User1", new UserData("User1", "Pass123", "Team A", new String[]{"Member 1", "Member 2", "Member 3", "Member 4", "Member 5"}));
+        usersData.put("User2", new UserData("User2", "Pass1234", "Team B", new String[]{"Member A", "Member B", "Member C", "Member D", "Member E"}));
+        usersData.put("User3", new UserData("User3", "Pass12345", "Team C", new String[]{"Member X", "Member Y", "Member Z", "Member W", "Member V"}));
+        usersData.put("User4", new UserData("User4", "Pass123456", "Team D", new String[]{"Member I", "Member II", "Member III", "Member IV", "Member V"}));
+ 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 1080, 673);
-        contentPane = new JPanel();
-        contentPane.setBackground(new Color(4, 18, 27));
-        contentPane.setBorder(null);
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+        contentBG = new JPanel();
+        contentBG.setBackground(new Color(4, 18, 27));
+        contentBG.setBorder(null);
+        setContentPane(contentBG);
+        contentBG.setLayout(null);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(255, 255, 255));
-        panel.setBounds(275, 131, 495, 372);
-        contentPane.add(panel);
-        panel.setLayout(null);
+        JPanel panelSignIn = new JPanel();
+        panelSignIn.setBackground(new Color(255, 255, 255));
+        panelSignIn.setBounds(275, 131, 495, 372);
+        contentBG.add(panelSignIn);
+        panelSignIn.setLayout(null);
 
         showPass = new JRadioButton("");
-        showPass.setHorizontalTextPosition(SwingConstants.LEADING);
+        showPass.setHorizontalTextPosition(SwingConstants.CENTER);
         showPass.setForeground(new Color(192, 192, 192));
         showPass.setIcon(new ImageIcon("C:\\Users\\Ryujin\\Downloads\\eye1.png"));
         showPass.setBackground(new Color(192, 192, 192));
-        showPass.setBounds(309, 184, 40, 16);
-        panel.add(showPass);
+        showPass.setBounds(309, 179, 40, 30);
+        panelSignIn.add(showPass);
 
         showPass.addItemListener(new ItemListener() {
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED) {
-                    passField.setEchoChar((char) 0); // Show the password
+                    pwdField.setEchoChar((char) 0); // Show the password
                 } else {
-                    passField.setEchoChar('\u2022'); // Hide the password
+                    pwdField.setEchoChar('\u2022'); // Hide the password
                 }
             }
         });
 
-        textSignIn = new JTextField();
-        textSignIn.setBackground(new Color(255, 255, 255));
-        textSignIn.setDisabledTextColor(new Color(255, 255, 255));
-        textSignIn.setEditable(false);
-        textSignIn.setHorizontalAlignment(SwingConstants.CENTER);
-        textSignIn.setBorder(null);
-        textSignIn.setFont(new Font("VALORANT", Font.PLAIN, 30));
-        textSignIn.setText("SIGN IN");
-        textSignIn.setBounds(162, 37, 167, 65);
-        panel.add(textSignIn);
-        textSignIn.setColumns(10);
+        txtSignIn = new JTextField();
+        txtSignIn.setBackground(new Color(255, 255, 255));
+        txtSignIn.setDisabledTextColor(new Color(255, 255, 255));
+        txtSignIn.setEditable(false);
+        txtSignIn.setHorizontalAlignment(SwingConstants.CENTER);
+        txtSignIn.setBorder(null);
+        txtSignIn.setFont(new Font("VALORANT", Font.PLAIN, 30));
+        txtSignIn.setText("SIGN IN");
+        txtSignIn.setBounds(162, 37, 167, 65);
+        panelSignIn.add(txtSignIn);
+        txtSignIn.setColumns(10);
         Color cc = new Color(253, 69, 86);
-        textSignIn.setForeground(cc);
+        txtSignIn.setForeground(cc);
 
         txtUser = new JTextField();
         txtUser.setHorizontalAlignment(SwingConstants.LEFT);
@@ -136,7 +137,7 @@ public class SignIn extends JFrame {
         txtUser.setBackground(new Color(192, 192, 192));
         txtUser.setBorder(null);
         txtUser.setBounds(141, 113, 208, 35);
-        panel.add(txtUser);
+        panelSignIn.add(txtUser);
         txtUser.setColumns(10);
         txtUser.addFocusListener(new FocusAdapter() {
             @Override
@@ -147,60 +148,72 @@ public class SignIn extends JFrame {
             }
         });
 
-        passField = new JPasswordField();
-        passField.setFont(new Font("Tungsten Bold", Font.PLAIN, 17));
-        passField.setBorder(null);
-        passField.setBackground(new Color(192, 192, 192));
-        passField.setBounds(141, 184, 208, 35);
-        panel.add(passField);
+        pwdField = new JPasswordField();
+        pwdField.setFont(new Font("Tungsten Bold", Font.PLAIN, 17));
+        pwdField.setBorder(null);
+        pwdField.setBackground(new Color(192, 192, 192));
+        pwdField.setBounds(141, 174, 208, 35);
+        panelSignIn.add(pwdField);
 
         JLabel lblFB = new JLabel("");
         lblFB.setBounds(141, 220, 40, 30);
         lblFB.setIcon(new ImageIcon("C:\\Users\\Ryujin\\Downloads\\facebook1.png"));
-        panel.add(lblFB);
+        panelSignIn.add(lblFB);
 
         JLabel lblGgl =  new JLabel("");
         lblGgl.setBounds(226, 220, 30, 30);
         lblGgl.setIcon(new ImageIcon("C:\\Users\\Ryujin\\Downloads\\google.png"));
-        panel.add(lblGgl);
+        panelSignIn.add(lblGgl);
 
         JLabel lblApl = new JLabel("");
         lblApl.setBounds(309, 208, 40, 53);
         lblApl.setIcon(new ImageIcon("C:\\Users\\Ryujin\\Downloads\\apple11.png"));
-        panel.add(lblApl);
-
+        panelSignIn.add(lblApl);
+        
+        userAdmin = new Hashtable<String, String>();
+        userAdmin.put("Admin1", "Admin12345");
+        
         JLabel lblEnter = new JLabel("");
         lblEnter.setBorder(new CompoundBorder());
         lblEnter.setBounds(205, 274, 75, 75);
         lblEnter.setIcon(new ImageIcon("C:\\Users\\Ryujin\\Downloads\\nextbutton3.png"));
-        panel.add(lblEnter);
+        panelSignIn.add(lblEnter);
         lblEnter.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if (e.getSource() == lblEnter) {
-                    String userText = txtUser.getText();
-                    char[] pwdText = passField.getPassword();
-                    String password = new String(pwdText);
+                String userText = txtUser.getText();
+                char[] pwdText = pwdField.getPassword();
+                String password = new String(pwdText);
 
-                    if (usersData.containsKey(userText)) {
-                        UserData userData = usersData.get(userText);
-                        String expectedPassword = userData.getPassword();
-                        if (password.equals(expectedPassword)) {
-                            JOptionPane.showMessageDialog(lblEnter, "Redirecting to User Home Page", "Login Successfully", 1);
-                            setVisible(false);
+                if (usersData.containsKey(userText)) {
+                    UserData userData = usersData.get(userText);
+                    String expectedPassword = userData.getPassword();
+                    if (password.equals(expectedPassword)) {
+                        JOptionPane.showMessageDialog(lblEnter, "Redirecting to User Home Page", "Login Successfully", 1);
+                        setVisible(false);
 
-                            UserHome userHomeFrame = new UserHome();
-                            userHomeFrame.updateLabels(userData); // Update the labels with user data
-                            userHomeFrame.setVisible(true);
-                            dispose();
-                        } else {
-                            JOptionPane.showMessageDialog(null, "Incorrect password for user. Please try again.");
-                        }
+                        UserHome userHomeFrame = new UserHome();
+                        userHomeFrame.updateLabels(userData); // Update the labels with user data
+                        userHomeFrame.setVisible(true);
+                        dispose();
                     } else {
-                        JOptionPane.showMessageDialog(null, "Incorrect username. Please try again.");
+                        JOptionPane.showMessageDialog(null, "Incorrect password for user. Please try again.");
                     }
+                } else if (userAdmin.containsKey(userText)) {
+                    String expectedAdminPassword = userAdmin.get(userText);
+
+                    if (password.equals(expectedAdminPassword)) {
+                        // If it's an admin, open the AdminHome window
+                        AdminHome adminHomeFrame = new AdminHome();
+                        adminHomeFrame.setVisible(true);
+                        dispose(); // Close the current window
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Incorrect password for admin. Please try again.");
+                    }
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect username. Please try again.");
                 }
             }
         });
-    }
+      }
 }
