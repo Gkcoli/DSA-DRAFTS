05052f5a35e4f3b4203110a9d95ac8d8a498afbb
Jerico Lim <111488202+vannicc@users.noreply.github.com>
2023-10-31 14:56:34 +0800

    Add files via upload

--- DRAFT/ExcelTempo/EditTourna.java
+++ DRAFT/ExcelTempo/EditTourna.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
@@ -37,6 +38,7 @@ public class EditTourna {
 	JoinTourna teamList = new JoinTourna();
 	FileHandler fileHandler = new FileHandler();
 	ExcelHandler excel = new ExcelHandler();
+	LinkedList<String> list;
 
 	/**
 	 * Launch the application.
@@ -80,7 +82,7 @@ public class EditTourna {
 		labelSelectTeam.setVerticalAlignment(SwingConstants.TOP);
 		labelSelectTeam.setForeground(new Color(189, 57, 68));
 		labelSelectTeam.setFont(new Font("Tungsten Bold", Font.PLAIN, 35));
-		labelSelectTeam.setBounds(46, 143, 147, 33);
+		labelSelectTeam.setBounds(462, 135, 147, 33);
 		frame.getContentPane().add(labelSelectTeam);
 		
 		JSpinner spinner = new JSpinner();
@@ -88,9 +90,9 @@ public class EditTourna {
 		LinkedList<String> teams = new LinkedList<>();
 			teams.addFirst("");
 			teams.addAll(ExcelHandler.fileReader(fileDirectory + "VanguardTeams.txt"));
-			System.out.println(teams);
+			
 		spinner.setModel(new SpinnerListModel(teams));
-		spinner.setBounds(194, 143, 226, 33);
+		spinner.setBounds(611, 135, 226, 33);
 		spinner.addChangeListener(new ChangeListener() {
 		public void stateChanged(ChangeEvent e) {
 			String spinnerValue = (String) spinner.getValue();
@@ -102,7 +104,7 @@ public class EditTourna {
 				txtField_T1M4.setText("");
 				txtField_T1M5.setText("");
 			} else {
-				LinkedList<String> list = new LinkedList<>();
+				list = new LinkedList<>();
 				try {
 					list.addAll(ExcelHandler.readExcelFile(ExcelHandler.checkUser(spinnerValue), 0));
 					txtTeamName.setText(list.get(0));
@@ -110,7 +112,7 @@ public class EditTourna {
 					txtField_T1M2.setText(list.get(2));
 					txtField_T1M3.setText(list.get(3));
 					txtField_T1M4.setText(list.get(4));
-					txtField_T1M5.setText(list.get(5));
+					txtField_T1M5.setText(list.get(5)); 
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -125,125 +127,196 @@ public class EditTourna {
 		lblTeams.setVerticalAlignment(SwingConstants.TOP);
 		lblTeams.setForeground(new Color(189, 57, 68));
 		lblTeams.setFont(new Font("Tungsten Bold", Font.PLAIN, 35));
-		lblTeams.setBounds(46, 201, 225, 33);
+		lblTeams.setBounds(462, 185, 91, 33);
 		frame.getContentPane().add(lblTeams);
 		
 		JLabel lblT1Name = new JLabel("TEAM NAME");
 		lblT1Name.setVerticalAlignment(SwingConstants.TOP);
 		lblT1Name.setForeground(new Color(0, 0, 0));
 		lblT1Name.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lblT1Name.setBounds(46, 237, 119, 33);
+		lblT1Name.setBounds(462, 232, 114, 33);
 		frame.getContentPane().add(lblT1Name);
 		
 		txtTeamName = new JTextField();
+		txtTeamName.setEditable(false);
 		txtTeamName.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtTeamName.setHorizontalAlignment(SwingConstants.LEFT);
-		txtTeamName.setBounds(175, 237, 245, 33);
+		txtTeamName.setBounds(586, 232, 251, 33);
 		frame.getContentPane().add(txtTeamName);
 		txtTeamName.setColumns(10);
 		
 		JLabel lblT1Mem = new JLabel("TEAM MEMBERS");
 		lblT1Mem.setForeground(Color.BLACK);
 		lblT1Mem.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lblT1Mem.setBounds(46, 273, 181, 33);
+		lblT1Mem.setBounds(462, 275, 147, 39);
 		frame.getContentPane().add(lblT1Mem);
 		
 		txtField_T1M1 = new JTextField();
+		txtField_T1M1.setEditable(false);
 		txtField_T1M1.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtField_T1M1.setColumns(10);
-		txtField_T1M1.setBounds(135, 300, 285, 33);
+		txtField_T1M1.setBounds(586, 315, 251, 33);
 		frame.getContentPane().add(txtField_T1M1);
 		
 		JLabel lbl1 = new JLabel("1. ");
 		lbl1.setVerticalAlignment(SwingConstants.TOP);
 		lbl1.setForeground(Color.BLACK);
 		lbl1.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lbl1.setBounds(108, 308, 17, 25);
+		lbl1.setBounds(559, 321, 28, 33);
 		frame.getContentPane().add(lbl1);
 		
 		JLabel lbl2 = new JLabel("2.");
 		lbl2.setVerticalAlignment(SwingConstants.TOP);
 		lbl2.setForeground(Color.BLACK);
 		lbl2.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lbl2.setBounds(108, 351, 17, 25);
+		lbl2.setBounds(559, 366, 17, 25);
 		frame.getContentPane().add(lbl2);
 		
 		JLabel lbl3 = new JLabel("3.");
 		lbl3.setVerticalAlignment(SwingConstants.TOP);
 		lbl3.setForeground(Color.BLACK);
 		lbl3.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lbl3.setBounds(108, 394, 17, 25);
+		lbl3.setBounds(558, 411, 17, 25);
 		frame.getContentPane().add(lbl3);
 		
 		JLabel lbl4 = new JLabel("4.");
 		lbl4.setVerticalAlignment(SwingConstants.TOP);
 		lbl4.setForeground(Color.BLACK);
 		lbl4.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lbl4.setBounds(108, 437, 17, 25);
+		lbl4.setBounds(559, 456, 21, 25);
 		frame.getContentPane().add(lbl4);
 		
 		JLabel lbl5 = new JLabel("5.");
 		lbl5.setVerticalAlignment(SwingConstants.TOP);
 		lbl5.setForeground(Color.BLACK);
 		lbl5.setFont(new Font("Tungsten Bold", Font.PLAIN, 30));
-		lbl5.setBounds(108, 480, 17, 25);
+		lbl5.setBounds(559, 501, 17, 25);
 		frame.getContentPane().add(lbl5);
 		
 		txtField_T1M2 = new JTextField();
+		txtField_T1M2.setEditable(false);
 		txtField_T1M2.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtField_T1M2.setColumns(10);
-		txtField_T1M2.setBounds(135, 343, 285, 33);
+		txtField_T1M2.setBounds(586, 360, 251, 33);
 		frame.getContentPane().add(txtField_T1M2);
 		
 		txtField_T1M3 = new JTextField();
+		txtField_T1M3.setEditable(false);
 		txtField_T1M3.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtField_T1M3.setColumns(10);
-		txtField_T1M3.setBounds(135, 386, 285, 33);
+		txtField_T1M3.setBounds(585, 405, 251, 33);
 		frame.getContentPane().add(txtField_T1M3);
 		
 		txtField_T1M4 = new JTextField();
+		txtField_T1M4.setEditable(false);
 		txtField_T1M4.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtField_T1M4.setColumns(10);
-		txtField_T1M4.setBounds(135, 429, 285, 33);
+		txtField_T1M4.setBounds(586, 450, 251, 33);
 		frame.getContentPane().add(txtField_T1M4);
 		
 		txtField_T1M5 = new JTextField();
+		txtField_T1M5.setEditable(false);
 		txtField_T1M5.setFont(new Font("Spiegel", Font.PLAIN, 15));
 		txtField_T1M5.setColumns(10);
-		txtField_T1M5.setBounds(135, 472, 285, 33);
+		txtField_T1M5.setBounds(586, 495, 251, 33);
 		frame.getContentPane().add(txtField_T1M5);
 		
 		
-		JButton btnPlayMatch = new JButton("PLAY MATCH");
-		btnPlayMatch.addMouseListener(new MouseAdapter() {
+		JButton btnHome = new JButton("HOME");
+		btnHome.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-//				PlayMatch window = new PlayMatch();
-//				window.frame.setVisible(true);
+				frame.dispose();
+				AdminHomePage backToAdmin = new AdminHomePage();
+				backToAdmin.setVisible(true);
 			}
 		});
-		btnPlayMatch.setVerticalAlignment(SwingConstants.BOTTOM);
-		btnPlayMatch.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
-		btnPlayMatch.setForeground(new Color(255, 255, 255));
-		btnPlayMatch.setBackground(new Color(189, 57, 68));
-		btnPlayMatch.setBounds(885, 135, 119, 39);
-		frame.getContentPane().add(btnPlayMatch);
+		btnHome.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnHome.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
+		btnHome.setForeground(new Color(255, 255, 255));
+		btnHome.setBackground(new Color(189, 57, 68));
+		btnHome.setBounds(885, 135, 119, 39);
+		frame.getContentPane().add(btnHome);
 		
-		JButton btnEditTeam = new JButton("EDIT TEAM");
+		JButton btnEditTeam = new JButton("EDIT LINEUP");
+		btnEditTeam.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				txtField_T1M1.setEditable(true);
+				txtField_T1M2.setEditable(true);
+				txtField_T1M3.setEditable(true);
+				txtField_T1M4.setEditable(true);
+				txtField_T1M5.setEditable(true);
+			}
+		}); 
 		btnEditTeam.setVerticalAlignment(SwingConstants.BOTTOM);
 		btnEditTeam.setForeground(Color.WHITE);
 		btnEditTeam.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
 		btnEditTeam.setBackground(new Color(189, 57, 68));
-		btnEditTeam.setBounds(531, 137, 119, 39);
+		btnEditTeam.setBounds(885, 195, 119, 39);
 		frame.getContentPane().add(btnEditTeam);
 		
-		JButton btnReplacePlayer = new JButton("SUB PLAYER");
-		btnReplacePlayer.setVerticalAlignment(SwingConstants.BOTTOM);
-		btnReplacePlayer.setForeground(Color.WHITE);
-		btnReplacePlayer.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
-		btnReplacePlayer.setBackground(new Color(189, 57, 68));
-		btnReplacePlayer.setBounds(531, 237, 119, 39);
-		frame.getContentPane().add(btnReplacePlayer);
+		JButton btnSaveMatch = new JButton("SAVE");
+		btnSaveMatch.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (txtTeamName.getText().isEmpty() || txtField_T1M1.getText().isEmpty()
+						|| txtField_T1M2.getText().isEmpty() || txtField_T1M3.getText().isEmpty()
+						|| txtField_T1M4.getText().isEmpty() || txtField_T1M5.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(btnSaveMatch, "Please input all credentials!");
+				} else {
+					list.clear();
+					list.add(txtTeamName.getText().toString());
+					list.add(txtField_T1M1.getText().toString());
+					list.add(txtField_T1M2.getText().toString());
+					list.add(txtField_T1M3.getText().toString());
+					list.add(txtField_T1M4.getText().toString());
+					list.add(txtField_T1M5.getText().toString());
+				
+					String filePath = ExcelHandler.checkUser(spinner.getValue().toString());
+					excel.writeAtSpecificColumn(filePath, 0, list);
+				
+					txtField_T1M1.setEditable(false);
+					txtField_T1M2.setEditable(false);
+					txtField_T1M3.setEditable(false);
+					txtField_T1M4.setEditable(false);
+					txtField_T1M5.setEditable(false);
+				}
+			}
+		});
+		btnSaveMatch.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnSaveMatch.setForeground(Color.WHITE);
+		btnSaveMatch.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
+		btnSaveMatch.setBackground(new Color(189, 57, 68));
+		btnSaveMatch.setBounds(885, 255, 119, 39);
+		frame.getContentPane().add(btnSaveMatch);
+		
+		JLabel lblDareToBe = new JLabel("DARE TO BE THE");
+		lblDareToBe.setForeground(new Color(189, 57, 68));
+		lblDareToBe.setFont(new Font("Tungsten Bold", Font.PLAIN, 45));
+		lblDareToBe.setBounds(60, 135, 204, 52);
+		frame.getContentPane().add(lblDareToBe);
+		
+		JLabel lblDifference = new JLabel("DIFFERENCE");
+		lblDifference.setFont(new Font("Tungsten Bold", Font.PLAIN, 99));
+		lblDifference.setBounds(58, 179, 351, 109);
+		frame.getContentPane().add(lblDifference);
+		
+		JButton btnPlayMatch = new JButton("PLAY MATCH");
+		btnPlayMatch.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnPlayMatch.setForeground(Color.WHITE);
+		btnPlayMatch.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
+		btnPlayMatch.setBackground(new Color(189, 57, 68));
+		btnPlayMatch.setBounds(885, 315, 119, 39);
+		frame.getContentPane().add(btnPlayMatch);
+		
+		JButton btnMatchSched = new JButton("MATCH SCHED");
+		btnMatchSched.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnMatchSched.setForeground(Color.WHITE);
+		btnMatchSched.setFont(new Font("Tungsten Bold", Font.PLAIN, 20));
+		btnMatchSched.setBackground(new Color(189, 57, 68));
+		btnMatchSched.setBounds(885, 376, 119, 39);
+		frame.getContentPane().add(btnMatchSched);
 		
 	}
 }
--- DRAFT/ExcelTempo/ExcelHandler.java
+++ DRAFT/ExcelTempo/ExcelHandler.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.LinkedList;
-
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -15,57 +14,49 @@ import org.apache.poi.ss.usermodel.Row;
 
 public class ExcelHandler {
 	String fileDirectory = "C:/Users/jeric/eclipse-workspace/TeleMastersValorantTournamentManager/src/";
-	public static void createSheet(LinkedList <String> list, String filePath, String teamName) {
-		try {  
-			//declare file name to be create   
-			String filename = filePath;  
-			//creating an instance of HSSFWorkbook class   
-			HSSFWorkbook workbook = new HSSFWorkbook();  
-			//invoking creatSheet() method and passing the name of the sheet to be created   
-			HSSFSheet sheet = workbook.createSheet(teamName);   
-		
-			//creating the 0th row using the createRow() method  
-			HSSFRow rowhead = sheet.createRow((short)0);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			rowhead.createCell(0).setCellValue(teamName);   
-			
-			//creating the 1st row using the createRow() method  
-			HSSFRow row1 = sheet.createRow((short)1);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			row1.createCell(0).setCellValue(list.get(1));   
-			
-			//creating the 2nd row using the createRow() method  
-			HSSFRow row2 = sheet.createRow((short)2);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			row2.createCell(0).setCellValue(list.get(2));  
-			
-			//creating the 3rd row using the createRow() method  
-			HSSFRow row3 = sheet.createRow((short)3);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			row3.createCell(0).setCellValue(list.get(3));  
-			
-			//creating the 4th row using the createRow() method  
-			HSSFRow row4 = sheet.createRow((short)4);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			row4.createCell(0).setCellValue(list.get(4));  
-			
-			//creating the 5th row using the createRow() method  
-			HSSFRow row5 = sheet.createRow((short)5);  
-			//creating cell by using the createCell() method and setting the values to the cell by using the setCellValue() method  
-			row5.createCell(0).setCellValue(list.get(5));  
-		
-			FileOutputStream fileOut = new FileOutputStream(filename);  
-			workbook.write(fileOut);  
-			//closing the Stream  
-			fileOut.close();  
-			//closing the workbook  
-			workbook.close();  
 	
-		} catch (Exception e) {  
-			e.printStackTrace();  
+	// Method that creates a file 
+	public void createFile(String filePath, int numRows, String sheetName) {
+		try   {  
+		//creating an instance of HSSFWorkbook class  
+		HSSFWorkbook workbook = new HSSFWorkbook();  
+		//invoking creatSheet() method and passing the name of the sheet to be created   
+		HSSFSheet sheet = workbook.createSheet(sheetName);    
+		Row row;
+		
+		for (int i = 0; i < numRows; i++) {
+			//creating the 0th row using the createRow() method  
+			// Create a new row and set its height
+	        row = sheet.createRow(i);
+		}
+		//creating the 0th row using the createRow() method  
+		// Create a new row and set its height
+        row = sheet.createRow(0);
+
+        // Write data to the excel file
+        row.createCell(0).setCellValue("Team");
+        row.createCell(1).setCellValue("K");
+        row.createCell(2).setCellValue("D");
+        row.createCell(3).setCellValue("A");
+        row.createCell(4).setCellValue("P");
+        row.createCell(5).setCellValue("DF");
+        
+		FileOutputStream fileOut = new FileOutputStream(filePath);  
+		workbook.write(fileOut);  
+		//closing the Stream  
+		fileOut.close();  
+		//closing the workbook  
+		workbook.close();  
+		//prints the message on the console  
+		System.out.println("Excel file has been generated successfully.");  
+		}   
+		catch (Exception e)   
+		{  
+		e.printStackTrace();  
 		}  
 	}
 	
+	// Method that reads a specific column in excel and inputs it to a linked list
 	public static LinkedList <String> readExcelFile(String fileName, int column) throws Exception {
 		LinkedList <String> names = new LinkedList<>();
 	        // Create a FileInputStream object to read the excel file
@@ -103,29 +94,7 @@ public class ExcelHandler {
 		return names;
 	}
 	
-	public static void addRowToExcel(String filePath, String sheetName, String input) {
-	    try {
-	        // Open the excel file
-	        FileInputStream fileInputStream = new FileInputStream(filePath);
-	        HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
-
-	        // Get the specified sheet
-	        HSSFSheet sheet = workbook.getSheet(sheetName);
-
-	        // Create a new row and add the input to the first cell
-	        Row newRow = sheet.createRow(sheet.getLastRowNum() + 1);
-	        newRow.createCell(0).setCellValue(input);
-
-	        // Save the changes to the excel file
-	        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-	        workbook.write(fileOutputStream);
-	        fileOutputStream.close();
-	        workbook.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
-	
+	// Method that writes elements of a linked list to a specific file
 	public void fileWriter(LinkedList <String> teamCreds, String filepath) {
 		try {
 		    FileWriter file = new FileWriter(filepath);
@@ -143,13 +112,14 @@ public class ExcelHandler {
 		    }
 	}
 	
+	// Method that reads through a file and inputs its content to a linked list
 	public static LinkedList<String> fileReader(String filePath) {
 		LinkedList<String> list = new LinkedList<>();
 			
 		try {
 			String str = "";
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
-			while ((str = reader.readLine()) != null) { // reads and stores file content to Stack 
+			while ((str = reader.readLine()) != null) { // reads and stores file content to LinkedList 
                 list.addLast(str);
             } 
 			reader.close();
@@ -159,9 +129,10 @@ public class ExcelHandler {
 		return list;
 	}
 	
+	// Method that returns a designated filePath from a user
 	public static String checkUser(String User) {
 		String filePath = "";
-		
+		 
 		if (User.equalsIgnoreCase("User1")) {
 			filePath = "C:/Users/jeric/eclipse-workspace/TeleMastersValorantTournamentManager/src/Team1.xlsx";
 		} else if (User.equalsIgnoreCase("User2")) {
@@ -176,4 +147,47 @@ public class ExcelHandler {
 		
 		return filePath;
 	}
+	
+	// Method that writes on a specific column
+	public void writeAtSpecificColumn(String excelFilePath, int column, LinkedList <String> list) {
+        
+		try {
+			FileInputStream inputStream = new FileInputStream(excelFilePath);
+			HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
+			HSSFSheet firstSheet = workbook.getSheetAt(0);
+			int totalRows = firstSheet.getPhysicalNumberOfRows();
+
+			for (int rowIndex = 0; rowIndex < totalRows; rowIndex++) {
+				Row row = firstSheet.getRow(rowIndex);
+				if (row == null) {
+					row = firstSheet.createRow(rowIndex);
+				}
+
+				
+				// Overwrite only a specific column (for example, column B)
+				int columnIndex = column; // 0-based index, so 1 means the second column
+				Cell cell = row.getCell(columnIndex);
+				if (cell == null) {
+					cell = row.createCell(columnIndex);
+				}
+				cell.setCellValue(list.get(rowIndex));
+				
+			}
+
+			inputStream.close();
+
+			FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+			workbook.write(outputStream);
+			outputStream.close();
+
+			workbook.close();
+			
+			System.out.println("Succesfully written to file");
+		} catch (Exception e) {
+			e.printStackTrace(); 
+		}
+	}   
+	
 }
+
+
