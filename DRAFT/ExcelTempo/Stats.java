8216ecf4d40a1916a6463b9238a4d5b682a10508
Jessy Cassandra <111488139+ysc4@users.noreply.github.com>
2023-11-10 21:26:09 +0800

    Update Stats.java

--- DRAFT/ExcelTempo/Stats.java
+++ DRAFT/ExcelTempo/Stats.java
@@ -4,20 +4,44 @@ import java.util.Queue;
 public class Stats {
     LinkedList <String> roundStats;
     ExcelHandler excel = new ExcelHandler();
-
     public Queue<String> Schedule = new LinkedList<>(); 
+    
 	
 	public Queue<String> getSched() {
 		return Schedule;
 	}
 	
-	public void setSched(String game) {
-		Schedule.add(game);
+	public void setSched() {
+		Schedule.add("GAME 1");
+		Schedule.add("GAME 2");
+		Schedule.add("GAME 3");
+		Schedule.add("GAME 4");
 	}
+
+    public void updateSched(String gameNum, LinkedList<String> sched, String t1, String t2, String status, String winner, String loser) {
+        if (Schedule.isEmpty()) {
+            status = "FINISHED";  }
+        else if(!((Schedule.peek()).equals(gameNum))) {
+            status = "FINISHED";
+        } else {
+            status = "WAITING FOR TEAMS";  }
+       
+        sched.add(t1);
+		sched.add(t2);
+		sched.add(status);
+		sched.add(winner);
+		sched.add(loser);
+    }
+
 	
 	public void removeFirst() {
 		Schedule.remove();
 	}
+
+    public String getFirst() {
+        String first = Schedule.peek();
+        return first;
+    }
     
     public LinkedList <String> getRoundStats() {
         return roundStats;
@@ -30,14 +54,16 @@ public class Stats {
     public String gameWinner(int t1, int t2, String winner, String team1, String team2) {
         if (t1 > t2) {
             winner = team1;
-        } else { winner = team2; }
+        }
+        else { winner = team2; }
         return winner;
     }
 
     public String gameLoser(String gameW, String team1, String team2, String loser) {
         if (gameW.equals(team1)) {
             loser = team2;
-        } else { loser = team1; }
+        } else {
+            loser = team1; }
         return loser;
     }
 }
