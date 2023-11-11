83eaa1e0ec5a298c7b16b13ad88ae63d6546a3ef
Franze Natividad <111488970+Barbatosss@users.noreply.github.com>
2023-11-11 12:23:48 +0800

    Update ExcelHandler.java

--- DRAFT/ExcelTempo/ExcelHandler.java
+++ DRAFT/ExcelTempo/ExcelHandler.java
@@ -200,7 +200,284 @@ public class ExcelHandler {
 		
 		return filePath;
 	}
+	public static String checkTeam(String Team) {
+		String filePath = "";
+		 
+		if (Team.equalsIgnoreCase("Team 1")) {
+			filePath = "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team1.xlsx";
+		} else if (Team.equalsIgnoreCase("Team 2")) {
+			filePath = "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team2.xlsx";
+		} else if (Team.equalsIgnoreCase("Team 3")) {
+			filePath = "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team3.xlsx";;
+		} else if (Team.equalsIgnoreCase("Team 4")) {
+			filePath = "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team4.xlsx";;
+		} else {
+			return "";
+		}
+		
+		return filePath;
+	}
 	
+	public String columnSum(String filePath, int colIndex) {
+	    int sum = 0;
+
+	    try {
+	        FileInputStream inputStream = new FileInputStream(filePath);
+	        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+	        XSSFSheet sheet = workbook.getSheetAt(0);
+
+	        // Iterate over each row in the sheet, starting from the second row
+	        for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+	            Row row = sheet.getRow(rowIndex);
+	            if (row != null) {
+	                Cell cell = row.getCell(colIndex);
+	                if (cell != null) {
+	                    if (cell.getCellType() == CellType.NUMERIC) {
+	                        // Check if the cell contains a numeric value
+	                        sum += (int) cell.getNumericCellValue();
+	                    } else if (cell.getCellType() == CellType.STRING) {
+	                        try {
+	                            // Try to parse the string as a numeric value
+	                            sum += Integer.parseInt(cell.getStringCellValue());
+	                        } catch (NumberFormatException e) {
+	                            // Handle the case where the string is not numeric
+	                            System.out.println("Non-numeric value found at row " + rowIndex + ", column " + colIndex);
+	                        }
+	                    }
+	                }
+	            }
+	        }
+
+	        inputStream.close();
+	        workbook.close();
+
+	        String result = String.valueOf(sum);
+	        return result;
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return ""; // Return an empty string if there's an error
+	}
+	//merge
+	public static void main(String[] args) {
+        LinkedList<LeaderEntry> leadersKills = new LinkedList<>();
+        LinkedList<LeaderEntry> leadersDefuses = new LinkedList<>();
+        LinkedList<LeaderEntry> leadersPlants = new LinkedList<>();
+        LinkedList<LeaderEntry> leadersSupport = new LinkedList<>();
+        LinkedList<LeaderEntry> leadersMatchMVP = new LinkedList<>();
+
+
+        String[] fileNames = {
+            "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team1.xlsx",
+            "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team2.xlsx",
+            "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team3.xlsx",
+            "C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/Team4.xlsx"
+        };
+
+        for (String fileName : fileNames) {
+            readExcelFile(fileName, leadersKills, leadersDefuses, leadersPlants, leadersSupport, leadersMatchMVP);
+        }
+
+        LeaderEntry maxKills = mergeSort(leadersKills, 2);
+        LeaderEntry maxDefuses = mergeSort(leadersDefuses, 6);
+        LeaderEntry maxPlants = mergeSort(leadersPlants, 5);
+        LeaderEntry maxSupport = mergeSort(leadersSupport, 4);
+        LeaderEntry MatchMVP = mergeSortMatchMVP(leadersMatchMVP);
+
+        writeResultsToExcel(maxKills, maxDefuses, maxPlants, maxSupport, MatchMVP);
+    }
+
+    public static void readExcelFile(String fileName, LinkedList<LeaderEntry> leadersKills, LinkedList<LeaderEntry> leadersDefuses, LinkedList<LeaderEntry> leadersPlants, LinkedList<LeaderEntry> leadersSupport, LinkedList<LeaderEntry> leadersMatchMVP) {
+        try {
+            FileInputStream excelFile = new FileInputStream(new File(fileName));
+            XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                String playerName = row.getCell(0).getStringCellValue();
+                int kills = readSpecificColumnInt(row, 1); // Read column 2 using index 1
+                int defuses = readSpecificColumnInt(row, 5); // Read column 5 using index 4
+                int plants = readSpecificColumnInt(row, 4);
+                int support = readSpecificColumnInt(row, 3);
+                int deaths = readSpecificColumnInt(row, 2);
+                int matchMVP = ((kills + support)- deaths);
+                
+                leadersKills.add(new LeaderEntry(playerName, kills, 0, 0, 0, 0)); // Initialize other values to 0
+                leadersDefuses.add(new LeaderEntry(playerName, 0, defuses, 0, 0, 0)); // Initialize other values to 0
+                leadersPlants.add(new LeaderEntry(playerName, 0, 0, plants, 0, 0)); // Initialize other values to 0
+                leadersSupport.add(new LeaderEntry(playerName, 0, 0, 0, support, 0)); // Initialize other values to 0
+                leadersMatchMVP.add(new LeaderEntry(playerName, kills, 0, 0, support, deaths));
+
+
+            }
+
+            excelFile.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int readSpecificColumnInt(Row row, int columnIndex) {
+        Cell cell = row.getCell(columnIndex);
+        if (cell != null) {
+            if (cell.getCellType() == CellType.NUMERIC) {
+                return (int) cell.getNumericCellValue();
+            } else if (cell.getCellType() == CellType.STRING) {
+                try {
+                    return Integer.parseInt(cell.getStringCellValue());
+                } catch (NumberFormatException e) {
+                    return 0; 
+                }
+            }
+        }
+        return 0; 
+    }
+
+    public static LeaderEntry mergeSort(LinkedList<LeaderEntry> leaders, int columnIndex) {
+        if (leaders.size() <= 1) {
+            return leaders.getFirst();
+        }
+
+        int mid = leaders.size() / 2;
+        LinkedList<LeaderEntry> left = new LinkedList<>(leaders.subList(0, mid));
+        LinkedList<LeaderEntry> right = new LinkedList<>(leaders.subList(mid, leaders.size()));
+
+        LeaderEntry maxLeft = mergeSort(left, columnIndex);
+        LeaderEntry maxRight = mergeSort(right, columnIndex);
+
+        if (columnIndex == 2) {
+            return (maxLeft.getKills() >= maxRight.getKills()) ? maxLeft : maxRight;
+        } else if (columnIndex == 6) {
+            return (maxLeft.getDefuses() >= maxRight.getDefuses()) ? maxLeft : maxRight;
+        } else if (columnIndex == 5) {
+            return (maxLeft.getPlants() >= maxRight.getPlants()) ? maxLeft : maxRight;
+        } else if (columnIndex == 4) {
+            return (maxLeft.getSupport() >= maxRight.getSupport()) ? maxLeft : maxRight;
+        } else {
+            return null;
+        }
+    }
+    
+    public static LeaderEntry mergeSortMatchMVP(LinkedList<LeaderEntry> leaders) {
+        if (leaders.size() <= 1) {
+            return leaders.getFirst();
+        }
+
+        int mid = leaders.size() / 2;
+        LinkedList<LeaderEntry> left = new LinkedList<>(leaders.subList(0, mid));
+        LinkedList<LeaderEntry> right = new LinkedList<>(leaders.subList(mid, leaders.size()));
+
+        LeaderEntry maxLeft = mergeSortMatchMVP(left);
+        LeaderEntry maxRight = mergeSortMatchMVP(right);
+
+        // Compare the values individually for Match MVP
+        int maxLeftValue = maxLeft.getKills() + maxLeft.getSupport() - maxLeft.getDeaths();
+        int maxRightValue = maxRight.getKills() + maxRight.getSupport() - maxRight.getDeaths();
+
+        return (maxLeftValue >= maxRightValue) ? maxLeft : maxRight;
+    }
+
+    
+    public static void writeResultsToExcel(LeaderEntry maxKills, LeaderEntry maxDefuses, LeaderEntry maxPlants, LeaderEntry maxSupport, LeaderEntry MatchMVP) {
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("LeagueLeaders");
+
+            // Create the header row
+            Row rowHeader = sheet.createRow(0);
+            rowHeader.createCell(0).setCellValue("Category");
+            rowHeader.createCell(1).setCellValue("Value");
+            rowHeader.createCell(2).setCellValue("Player Name");
+
+            // Create the max defuses row
+            Row rowMaxDefuse = sheet.createRow(1);
+            rowMaxDefuse.createCell(0).setCellValue("Defuse");
+            rowMaxDefuse.createCell(1).setCellValue(maxDefuses.getDefuses());
+            rowMaxDefuse.createCell(2).setCellValue(maxDefuses.getPlayerName());
+
+            // Create the max kills row
+            Row rowMaxKills = sheet.createRow(2);
+            rowMaxKills.createCell(0).setCellValue("Kills");
+            rowMaxKills.createCell(1).setCellValue(maxKills.getKills());
+            rowMaxKills.createCell(2).setCellValue(maxKills.getPlayerName());
+            
+            // Create the plants row
+            Row rowMaxPlants = sheet.createRow(3);
+            rowMaxPlants.createCell(0).setCellValue("Plants");
+            rowMaxPlants.createCell(1).setCellValue(maxPlants.getPlants());
+            rowMaxPlants.createCell(2).setCellValue(maxPlants.getPlayerName());
+            
+            // Create the max support row
+            Row rowMaxSupport = sheet.createRow(4);
+            rowMaxSupport.createCell(0).setCellValue("Support");
+            rowMaxSupport.createCell(1).setCellValue(maxSupport.getSupport());
+            rowMaxSupport.createCell(2).setCellValue(maxSupport.getPlayerName());
+            
+            
+            Row rowMatchMVP = sheet.createRow(5);
+            rowMatchMVP.createCell(0).setCellValue("Match MVP");
+            rowMatchMVP.createCell(1).setCellValue(MatchMVP.getKills());
+            rowMatchMVP.createCell(2).setCellValue(MatchMVP.getDeaths());
+            rowMatchMVP.createCell(3).setCellValue(MatchMVP.getSupport());
+            rowMatchMVP.createCell(4).setCellValue(MatchMVP.getPlayerName());
+
+            // Save the workbook to a file
+            FileOutputStream outputStream = new FileOutputStream("C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/LeagueLeaders.xlsx");
+            workbook.write(outputStream);
+            outputStream.close();
+            workbook.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //end of merge
+    public void updateLabels(LeagueStats leagueStats) {
+        try (FileInputStream fis = new FileInputStream("C:/Users/carlo/OneDrive/Desktop/pransue/School/FIRST YEAR/ECLIPSE/telemasters/src/LeagueLeaders.xlsx");
+             XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+            XSSFSheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+
+            // Replace these variables with actual cell locations
+            String mostKillsName = getCellValue(sheet.getRow(2).getCell(2));
+            String mostKillCount = getCellValue(sheet.getRow(2).getCell(1));
+            String mostPlantsName = getCellValue(sheet.getRow(3).getCell(2));
+            String mostPlantCount = getCellValue(sheet.getRow(3).getCell(1));
+            String supportName = getCellValue(sheet.getRow(4).getCell(2));
+            String supporAssistCount = getCellValue(sheet.getRow(4).getCell(1));
+            String mostDefusesName = getCellValue(sheet.getRow(1).getCell(2));
+            String mostDefusesCount = getCellValue(sheet.getRow(1).getCell(1));
+            String matchMVPName =  getCellValue(sheet.getRow(5).getCell(4));
+            String matchMVPKDA = ( getCellValue(sheet.getRow(5).getCell(1)) + "/" + getCellValue(sheet.getRow(5).getCell(2)) + "/" +  getCellValue(sheet.getRow(5).getCell(3)));
+            
+            // Update the JLabels in LeagueStats
+            leagueStats.updateLabels(mostKillsName, mostKillCount, mostPlantsName, mostPlantCount, supportName, supporAssistCount, mostDefusesName, mostDefusesCount, matchMVPName, matchMVPKDA );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String getCellValue(Cell cell) {
+        if (cell != null) {
+            CellType cellType = cell.getCellType();
+            switch (cellType) {
+                case STRING:
+                    return cell.getStringCellValue();
+                case NUMERIC:
+                    // Check if it's a whole number (integer)
+                    if (DateUtil.isCellDateFormatted(cell) || cell.getNumericCellValue() != (int) cell.getNumericCellValue()) {
+                        return String.valueOf(cell.getNumericCellValue());
+                    } else {
+                        return String.valueOf((int) cell.getNumericCellValue());
+                    }
+                default:
+                    return ""; 
+            }
+        }
+        return "";
+    }
+
 	// Method that writes on a specific column
 	public void writeAtSpecificColumn(String excelFilePath, int column, LinkedList <String> list) {
         
